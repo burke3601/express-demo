914d8c47747faf109f554e4c67d3462ecf811eb9
Will Burke <billyburke1012@gmail.com>
2020-12-30 15:38:49 -0500

    updates to films

--- index.js
+++ index.js
@@ -15,17 +15,37 @@ app.set('view engine', 'html');
 
 
 const PORT = 3000;
-const data = require("./data.json")
+//const data = require("./data.json")
 const homeRouter = require('./routers/homeRouter');
+const movieRouter = require('./routers/movieRouter')
 
 //render data from data.json to template
 
 app.use(logger);
 app.use('/', homeRouter);
+app.use('/films', movieRouter)
 
-app.get('/movies', (req, res) =>{
-    console.log(data);
-    // const movieHtmlArray = [];
+// app.get('/movies', (req, res) =>{
+//     console.log(data);
+    
+//     res.render('movieList',{
+//         locals: {
+//             movies: data
+//         }
+//     });
+// } )
+
+server.listen(PORT, () => {
+    console.log(`listening at port ${PORT}`)
+});
+
+
+
+
+
+
+//previously this was in the app.get(/movies)
+// const movieHtmlArray = [];
     // for (let d of data) {
     //     movieHtmlArray.push(`<p>${d.title}</p>`)
     // }
@@ -36,14 +56,3 @@ app.get('/movies', (req, res) =>{
 
     // const movieHtmlString = data.map(d => `<p>!${d.title}!!!</p>`).join('');
     // console.log(movieHtmlString);
-
-    res.render('movieList',{
-        locals: {
-            movies: data
-        }
-    });
-} )
-
-server.listen(PORT, () => {
-    console.log(`listening at port ${PORT}`)
-});
---
+++ routers/movieRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { movieHandler } = require('../controllers/movieController')
+
+router.get('/', movieHandler);
+
+module.exports = router;
