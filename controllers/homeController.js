e89658be9374e1fe63923181868b900da4c167cd
Will Burke <billyburke1012@gmail.com>
2020-12-30 15:04:58 -0500

    add templates for movies

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,6 @@
 const homeHandler = (req, res) => {
-    res.send(`<h1>Hello World from controller!</h1>`)
+   // res.send(`<h1>Hello World from controller!</h1>`)
+    res.render('homepage')
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
+const  es6Renderer = require('express-es6-template-engine');
+
+
 
 const app = express();
 const server = http.createServer(app);
 const logger = morgan('tiny');
 
+app.engine('html',es6Renderer );
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
 
 const PORT = 3000;
-
+const data = require("./data.json")
 const homeRouter = require('./routers/homeRouter');
 
-// app.get('/', (req,res) => {
-//     res.send(`<h1>Hello World!</h1>`)
-// })
+//render data from data.json to template
 
 app.use(logger);
 app.use('/', homeRouter);
+
+app.get('/movies', (req, res) =>{
+    console.log(data);
+    // const movieHtmlArray = [];
+    // for (let d of data) {
+    //     movieHtmlArray.push(`<p>${d.title}</p>`)
+    // }
+  
+
+    // const movieHtmlArray = data.map(d => `<p>${d.title}!!!</p>`);
+    // const movieHtmlString = movieHtmlArray.join('');
+
+    // const movieHtmlString = data.map(d => `<p>!${d.title}!!!</p>`).join('');
+    // console.log(movieHtmlString);
+
+    res.render('movieList',{
+        locals: {
+            movies: data
+        }
+    });
+} )
 
 server.listen(PORT, () => {
     console.log(`listening at port ${PORT}`)
